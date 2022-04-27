6cdc7a3aef9b56dd1ad9e10eb6472b79292705e6
Saga Rudolphi <Saga.Rudolphi@sigma.se>
2022-04-27 21:06:13 +0200

    3.6 Phonebook backend part6

--- index.js
+++ index.js
@@ -4,55 +4,54 @@ const app = express()
 
 app.use(express.json())
 
+
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-  ]
-
-
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Welcome to my phonebook</h1>')
-  })
-  
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
+  response.send('<h1>Welcome to my phonebook</h1>')
+})
 
-  
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
 app.get('/info', (request, response) => {
-    response.send(`<p>This phonebook has info of ${Object.keys(persons).length} contacts.</p> <p> ${new Date()}</p>`)
+  response.send(`<p>This phonebook has info of ${Object.keys(persons).length} contacts.</p> <p> ${new Date()}</p>`)
 })
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id=== id)
-  
+  const person = persons.find(person => person.id === id)
+
   if (person) {
-    response.json(person)  
+    response.json(person)
   }
-  else{
+  else {
     response.statusMessage = "The person was not found"
     response.status(404).end()
-   }
+  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -63,12 +62,30 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body  
-  const id =  Math.floor(Math.random() * 5000)
+  const body = request.body
+  const id = Math.floor(Math.random() * 5000)
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name is missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Number is missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'The name must be unique'
+    })
+  }
 
   const person = {
     id: id,
-    name: body.name, 
+    name: body.name,
     number: body.number
   }
 
