666407fb98255e5ccc0de59f4ca8d413c91b4a4d
Saga Rudolphi <Saga.Rudolphi@sigma.se>
2022-05-07 22:52:22 +0200

    3.20 Phonebook database, step 8

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/api/persons/:id', (request, response, next) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -80,6 +80,12 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
+  if(Person.findById(request.params.id)) {
+    return response.status(400).json({
+      error: 'Person already exists. Try PUT instead.'
+    })
+  }
+
   const person = new Person({
     name: body.name,
     number: body.number
@@ -88,6 +94,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -106,7 +113,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, body, { new: true, runValidators: true, context: 'query' })
   .then(updatedPerson => {
     response.json(updatedPerson)
   })
@@ -124,6 +131,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
